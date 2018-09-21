fca29e660298de937f7b8006025409f4b9fb2ef4
Nick Jaton <nickajaton@gmail.com>
2018-09-20 21:16:45 -0500

    new table

--- peopledata.sql
+++ peopledata.sql
@@ -4,10 +4,8 @@ INSERT INTO `peopledata`.`new_table`
 (`ID`,`Name`,`Age`,`Job`,`Salary`)
 VALUES (1,'tom',22,'Retail Manager',24000);
 
-SELECT 
-    *
-FROM
-    peopledata.new_table;
+SELECT *
+FROM peopledata.new_table;
 
 SET SQL_SAFE_UPDATES = 0;
 DELETE FROM peopledata.new_table 
@@ -41,16 +39,12 @@ values
 	(19,'Anna', 38, 'Police Officier',104000);
     
 UPDATE peopledata.new_table 
-SET 
-    Name = 'Tom'
-WHERE
-    id = '1';
+SET Name = 'Tom'
+WHERE id = '1';
 
 UPDATE peopledata.new_table 
-SET 
-    Job = 'Database Admin'
-WHERE
-    ID = '12';
+SET Job = 'Database Admin'
+WHERE ID = '12';
 
 alter table peopledata.new_table
 add column Degree varchar(60) after Age;
@@ -81,34 +75,29 @@ values
 on duplicate key update `id`=values(`id`), Degree=values(`Degree`);
 
 
-SELECT 
-    AVG(salary)
-FROM
-    peopledata.new_table;SELECT 
-    MIN(salary)
-FROM
-    peopledata.new_table;SELECT 
-    MAX(salary)
-FROM
-    peopledata.new_table;SELECT 
-    STDDEV(salary)
-FROM
-    peopledata.new_table;SELECT 
-    COUNT(*)
-FROM
-    peopledata.new_table
-WHERE
-    Degree = 'BS';SELECT 
-    AVG(salary)
-FROM
-    peopledata.new_table
-WHERE
-    degree = 'BS';SELECT 
-    AVG(salary)
-FROM
-    peopledata.new_table
-WHERE
-    degree = 'BS' AND age > '24';CREATE TABLE `peopledata`.`Second_Table` (
+SELECT AVG(salary)
+FROM peopledata.new_table;
+    
+SELECT MIN(salary)
+FROM peopledata.new_table;
+
+SELECT MAX(salary)
+FROM peopledata.new_table;
+
+SELECT STDDEV(salary)
+FROM peopledata.new_table;
+
+SELECT COUNT(*)
+FROM peopledata.new_table
+WHERE Degree = 'BS';
+
+SELECT AVG(salary)
+FROM peopledata.new_table
+WHERE degree = 'BS';
+
+SELECT AVG(salary)
+FROM peopledata.new_table
+WHERE degree = 'BS' AND age > '24';CREATE TABLE `peopledata`.`Second_Table` (
     `idnew_table` INT NOT NULL,
     `House / Apartment` VARCHAR(45) NOT NULL,
     `Home_Value` INT NOT NULL,
@@ -139,61 +128,62 @@ value
     (18, 'Apartment', NULL),
     (19, 'House', '175000');
     
-SELECT 
-    *
-FROM
-    peopledata.Second_Table;
+SELECT *
+FROM peopledata.Second_Table;
 
-SELECT 
-    *
-FROM
-    peopledata.new_table
-        INNER JOIN
-    peopledata.Second_Table ON ID = idnew_table;
+SELECT *
+FROM peopledata.new_table
+INNER JOIN peopledata.Second_Table ON ID = idnew_table;
 
-SELECT 
-    ID, Age, Salary, Home_Value
-FROM
-    peopledata.new_table
-        LEFT OUTER JOIN
-    peopledata.Second_Table ON ID = idnew_table
-WHERE
-    Home_Value IS NOT NULL
-        AND salary >= 60000
+SELECT ID, Age, Salary, Home_Value
+FROM peopledata.new_table
+LEFT OUTER JOIN peopledata.Second_Table ON ID = idnew_table
+WHERE Home_Value IS NOT NULL AND salary >= 60000
 GROUP BY ID DESC;
 
-SELECT 
-    AVG(Salary)
-FROM
-    peopledata.new_table
-WHERE
-    ID IN (SELECT 
-            idnew_table
-        FROM
-            peopledata.Second_Table
-        WHERE
-            `House / Apartment` IS NOT NULL)
-        AND age <= 40;SELECT 
-    AVG(Age)
-FROM
-    peopledata.new_table
-WHERE
-    ID IN (SELECT 
-            idnew_table
-        FROM
-            peopledata.Second_Table
-        WHERE
-            `Home_Value` >= 120
-                AND `Home_Value` <= 200000);SELECT 
-    STDDEV(Age)
-FROM
-    peopledata.new_table
-WHERE
-    ID IN (SELECT 
-            idnew_table
-        FROM
-            peopledata.Second_Table
-        WHERE
-            `Home_Value` >= 120
-                AND `Home_Value` <= 200000); #6.5422
-                
+SELECT AVG(Salary)
+FROM peopledata.new_table
+WHERE ID IN (SELECT idnew_table
+				FROM peopledata.Second_Table
+                WHERE `House / Apartment` IS NOT NULL) AND age <= 40;
+SELECT AVG(Age)
+FROM peopledata.new_table
+WHERE ID IN (SELECT idnew_table
+			FROM peopledata.Second_Table
+			WHERE `Home_Value` >= 120 AND `Home_Value` <= 200000);
+            
+SELECT STDDEV(Age)
+FROM peopledata.new_table
+WHERE ID IN (SELECT idnew_table
+			FROM peopledata.Second_Table
+            WHERE `Home_Value` >= 120 AND `Home_Value` <= 200000); #6.5422
+            
+ALTER TABLE `peopledata`.`3rd_Table` 
+CHANGE COLUMN `Langauge of Choice` `Langauge of Choice` VARCHAR(45) NULL ;
+
+insert into `peopledata`.`3rd_Table` (`id3rd_Table`,`Pet Amount`,`Computer`,`Langauge of Choice`)
+value
+	(1, '0', 'MAC',NULL),
+    (2, '1', 'PC', NULL),
+    (3, '5', 'PC', 'JAVA'),
+    (4, '0', 'PC', 'R'),
+    (5, '0', 'MAC', NULL),
+    (6, '4', 'MAC', 'SQL'),
+    (7, '1', 'PC', 'SQL'),
+    (8, '2', 'PC', NULL),
+    (9, '2', 'PC', NULL),
+    (10, '4', 'MAC', 'PHP'),
+    (11, '1', 'PC', 'MATLAB'),
+    (12, '3', 'MAC', 'SQL'),
+    (13, '0', 'PC', 'MATLAB'),
+    (14, '0', 'PC', NULL),
+    (15, '1', 'MAC', 'ASSEMBLY'),
+    (16, '2', 'PC', 'PYTHON'),
+    (17, '0', 'PC', NULL),
+    (18, '1', 'MAC', NULL),
+    (19, '0', 'PC', 'ARDUINO');
+
+RENAME TABLE `peopledata`.`3rd_Table` TO `peopledata`.`Third_Table`;
+		
+SELECT * 
+FROM peopledata.Third_Table;
